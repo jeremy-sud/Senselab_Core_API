fd70cf0436c64a517397c9343949113941aa88c5
JeremyAlva <deadmooncr@gmail.com>
2026-05-10 03:13:58 -0600

    chore: update project dependencies and refactor PsySH interactive readline components

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit35371eddcc5bc8a3c19e74daaaf675de
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit35371eddcc5bc8a3c19e74daaaf675de', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit35371eddcc5bc8a3c19e74daaaf675de', 'loadClassLoader'));
